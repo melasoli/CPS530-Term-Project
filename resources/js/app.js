eb7b540cc96dcf5424b9c4f1d62d8af272942306
melasoli <49106525+melasoli@users.noreply.github.com>
2022-05-09 21:17:02 -0400

    Add gallery component

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,7 @@
 require('./bootstrap');
 
 window.Vue = require('vue').default;
+Vue.component('example-component', require('./components/Gallery.vue').default);
 
 /**
  * The following block of code may be used to automatically register your
