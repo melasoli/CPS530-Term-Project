84887f6fade400be85c3742c84414802777b9ae8
melasoli <49106525+melasoli@users.noreply.github.com>
2022-05-09 21:22:25 -0400

    Add navbar

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,6 @@
 require('./bootstrap');
 
 window.Vue = require('vue').default;
-Vue.component('example-component', require('./components/Gallery.vue').default);
 
 /**
  * The following block of code may be used to automatically register your
@@ -20,7 +19,9 @@ Vue.component('example-component', require('./components/Gallery.vue').default);
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('navi', require('./components/Navi.vue').default);
+
+Vue.component('gallery', require('./components/Gallery.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
