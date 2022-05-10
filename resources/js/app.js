1246cd45f587d64c61ff02125d080421cc4fe808
melasoli <49106525+melasoli@users.noreply.github.com>
2022-05-09 22:26:07 -0400

    Remove default from registered components

--- resources/js/app.js
+++ resources/js/app.js
@@ -19,9 +19,9 @@ window.Vue = require('vue').default;
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('navi', require('./components/Navi.vue').default);
+Vue.component('navi', require('./components/Navi.vue'));
 
-Vue.component('gallery', require('./components/Gallery.vue').default);
+Vue.component('gallery', require('./components/Gallery.vue'));
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
